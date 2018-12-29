38ac0c1592a35a53f11ef4e9c12758e4fb57c461
yuqian <qian.yu@deephi.tech>
2018-12-29 18:00:40 +0800

    fix bugs

--- profile/dprof.js
+++ profile/dprof.js
@@ -1,12 +1,4 @@
-function dprof() {return [
-    {
-        name:types[0].name,
-        itemStyle:{normal: {color: types[0].color}},
-        value:[0,0,200,200]
-    },
-    {
-        name:"cccc",
-        itemStyle:{normal: {color: types[1].color}},
-        value:[1,0,100,100]
-    }
+function dprof() { return [
+    {name:instrs['name'][0],itemStyle:{normal: {color:instrs['color'][0]}},value:[0,0,200,200]},
+    {name:instrs['name'][1],itemStyle:{normal: {color:instrs['color'][1]}},value:[1,0,200,200]}
 ];};
