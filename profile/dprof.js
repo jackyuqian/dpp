53cc3194dcaf40dfad870add6e16151b422da77e
yuqian <qian.yu@deephi.tech>
2018-12-29 17:13:19 +0800

    add profile

---
+++ profile/dprof.js
@@ -0,0 +1,12 @@
+function dprof() {return [
+    {
+        name:types[0].name,
+        itemStyle:{normal: {color: types[0].color}},
+        value:[0,0,200,200]
+    },
+    {
+        name:"cccc",
+        itemStyle:{normal: {color: types[1].color}},
+        value:[1,0,100,100]
+    }
+];};
